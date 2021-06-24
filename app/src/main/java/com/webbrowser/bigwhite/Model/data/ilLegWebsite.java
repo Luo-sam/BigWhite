098ccfc08927a14de799ed7885384884062a4652
MOSOJO866 <473405221@qq.com>
2021-06-24 10:27:36 +0800

    非法拦截

---
+++ app/src/main/java/com/webbrowser/bigwhite/Model/data/ilLegWebsite.java
@@ -0,0 +1,11 @@
+package com.webbrowser.bigwhite.Model.data;
+
+public class ilLegWebsite {
+    public static  String[] illegWebsite(){
+        String[] data=new String[3];
+        data[0]="https://www.bilibili.com/";
+        data[1]="https://www.google.com/";
+        data[2]="http://www.qq.com/";
+        return data;
+    }
+}
