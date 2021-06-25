281f27cd53dacf8e6daf0c1229089683fd837cd1
Luo-sam <2532467954@qq.com>
2021-06-25 10:48:18 +0800

    url的获取

--- app/src/main/java/com/webbrowser/bigwhite/View/fragment/SearchFragment.java
+++ app/src/main/java/com/webbrowser/bigwhite/View/fragment/SearchFragment.java
@@ -44,6 +44,7 @@ import com.webbrowser.bigwhite.Model.data.ilLegWebsite;
 import com.webbrowser.bigwhite.Model.data.responseData_put;
 import com.webbrowser.bigwhite.R;
 import com.webbrowser.bigwhite.View.adapter.searchHistoryAdapter;
+import com.webbrowser.bigwhite.activity.infoDetail;
 import com.webbrowser.bigwhite.activity.login;
 import com.webbrowser.bigwhite.utils.httpUtils;
 import com.webbrowser.bigwhite.widget.MingWebView;
@@ -91,13 +92,12 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
     private String token;
 
 
-
     /*历史记录*/
     private historyDao history;
 
 
-    private static final String HTTP="http://";
-    private static final String HTTPS="https://";
+    private static final String HTTP = "http://";
+    private static final String HTTPS = "https://";
 
     private Activity mActivity;
 
@@ -120,16 +120,17 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
     public WebView getWebView() {
         return webView;
     }
+
     @Override
     public void onAttach(@NonNull Activity activity) {
         super.onAttach(activity);
-        mActivity=activity;
+        mActivity = activity;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.web_search,container,false);
+        view = inflater.inflate(R.layout.web_search, container, false);
         sc = new RecordsDao(mActivity);
         initView(view);
         initWeb();
@@ -137,7 +138,6 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
         initHistory();
         return view;
     }
-
 
 
     private void initHistory() {
@@ -149,25 +149,25 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
         data = new ArrayList<>();
         temList = sc.getRecordsList();
         reversedList();
-        searchHistoryAdapter scAdapter = new searchHistoryAdapter(mActivity,data);
-        ListView scList  = view.findViewById(R.id.sc_history_list);
+        searchHistoryAdapter scAdapter = new searchHistoryAdapter(mActivity, data);
+        ListView scList = view.findViewById(R.id.sc_history_list);
         scList.setAdapter(scAdapter);
 
         scList.setOnItemClickListener((parent, view, position, id) -> {
             textUrl.setText(data.get(position));
             btnStart.callOnClick();
             textUrl.clearFocus();
-            Toast.makeText(mActivity,data.get(position),Toast.LENGTH_SHORT).show();
+            Toast.makeText(mActivity, data.get(position), Toast.LENGTH_SHORT).show();
         });
 
     }
 
     /*初始化得到的view*/
-    private void initView(View view){
+    private void initView(View view) {
         //mainActivity=(MainActivity)getActivity();
-        illegWebsite=view.findViewById(R.id.illeg);
+        illegWebsite = view.findViewById(R.id.illeg);
         illegWebsite.setVisibility(View.GONE);
-        textView=view.findViewById(R.id.goback);
+        textView = view.findViewById(R.id.goback);
 
         /*对两个页面的初始化*/
         Liner_search = view.findViewById(R.id.linearLayout);
@@ -178,13 +178,13 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
 
 
         /*键盘事件绑定*/
-        manager=(InputMethodManager) requireContext().getSystemService(Activity.INPUT_METHOD_SERVICE);
+        manager = (InputMethodManager) requireContext().getSystemService(Activity.INPUT_METHOD_SERVICE);
         /*搜索ID的获取*/
-        webView= view.findViewById(R.id.webView);
-        btnStart= view.findViewById(R.id.btnStart);
-        textUrl= view.findViewById(R.id.textUrl);
-        progressBar= view.findViewById(R.id.progressBar);
-        webIcon= view.findViewById(R.id.webIcon);
+        webView = view.findViewById(R.id.webView);
+        btnStart = view.findViewById(R.id.btnStart);
+        textUrl = view.findViewById(R.id.textUrl);
+        progressBar = view.findViewById(R.id.progressBar);
+        webIcon = view.findViewById(R.id.webIcon);
         TextView clear = view.findViewById(R.id.clear);
         /*绑定点击事件*/
         btnStart.setOnClickListener(this);
@@ -195,7 +195,7 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
         textView.setOnClickListener(this);
         //地址输入栏获取和失去焦点处理
         textUrl.setOnFocusChangeListener((popView, hasFocus) -> {
-            if(hasFocus){
+            if (hasFocus) {
                 //设置聚焦时为网址
                 textUrl.setText(webView.getUrl());
                 //显示back的图标
@@ -206,7 +206,7 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
                 web.setVisibility(View.GONE);
                 searchHis.setVisibility(View.VISIBLE);
 
-            }else{
+            } else {
                 //显示网站名
                 textUrl.setText(webView.getTitle());
                 //显示网站图标
@@ -219,7 +219,7 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
             }
         });
         textUrl.setOnKeyListener((v, keyCode, event) -> {
-            if(keyCode== KeyEvent.KEYCODE_ENTER &&event.getAction()== KeyEvent.ACTION_DOWN){
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
                 //执行搜索
                 btnStart.callOnClick();
                 textUrl.clearFocus();
@@ -230,12 +230,12 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
 
     /*初始化webView*/
     @SuppressLint("SetJavaScriptEnabled")
-    private void initWeb(){
+    private void initWeb() {
         webView.setWebViewClient(new MkWebViewClient());
         webView.setWebChromeClient(new MkWebChromeClient());
-        WebSettings settings=webView.getSettings();
+        WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
-        settings.setUserAgentString(settings.getUserAgentString()+"mkBrowser");
+        settings.setUserAgentString(settings.getUserAgentString() + "mkBrowser");
         settings.setUseWideViewPort(true);
         settings.setLoadWithOverviewMode(true);
         settings.setSupportZoom(true);
@@ -248,20 +248,21 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
         settings.setDefaultTextEncodingName("utf-8");
         settings.setDomStorageEnabled(true);
         settings.setPluginState(WebSettings.PluginState.ON);
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
         }
         webView.loadUrl(getResources().getString(R.string.home_url));
+
     }
 
     private class MkWebViewClient extends WebViewClient {
         @Override
-        public boolean shouldOverrideUrlLoading(WebView view,String url){
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
             //设置在webView点击打开的新网页在当前界面显示，而不跳转到新的浏览器中
-            if(url==null){
+            if (url == null) {
                 return true;
             }
-            if(url.startsWith(HTTP)||url.startsWith(HTTPS)){
+            if (url.startsWith(HTTP) || url.startsWith(HTTPS)) {
                 view.loadUrl(url);
                 return true;
             }
@@ -270,17 +271,18 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
                 startActivity(intent);
                 return true;
-            }catch (Exception e){
+            } catch (Exception e) {
                 return true;
             }
         }
+
         @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon){
-            String[] data=new String[3];
-            data= ilLegWebsite.illegWebsite();
-            for(int i=0;i< data.length;i++){
-                if(url.equals(data[i])){
-                    Toast.makeText(getActivity(),"非法网站",Toast.LENGTH_LONG).show();
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            String[] data = new String[3];
+            data = ilLegWebsite.illegWebsite();
+            for (int i = 0; i < data.length; i++) {
+                if (url.equals(data[i])) {
+                    Toast.makeText(getActivity(), "非法网站", Toast.LENGTH_LONG).show();
                     view.stopLoading();
                     Liner_search.setVisibility(View.GONE);
                     web.setVisibility(View.GONE);
@@ -289,7 +291,7 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
 
                 }
             }
-            super.onPageStarted(view,url,favicon);
+            super.onPageStarted(view, url, favicon);
             // 网页开始加载，显示进度条
             progressBar.setProgress(0);
             progressBar.setVisibility(View.VISIBLE);
@@ -298,9 +300,16 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
             // 切换默认网页图标
             webIcon.setImageResource(R.drawable.internet);
         }
+
         @Override
-        public void onPageFinished(WebView view,String url){
-            super.onPageFinished(view,url);
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            if (view.getUrl().contains("https://www.baidu.com/#iact=wiseindex%")) {
+                Intent intent = new Intent();
+                intent.setClass(mActivity,infoDetail.class);
+                intent.putExtra("url",view.getUrl());
+                startActivityForResult(intent,123);
+            }
             // 网页加载完毕，隐藏进度条
             progressBar.setVisibility(View.INVISIBLE);
             // 改变标题
@@ -315,10 +324,10 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
                     String name = textUrl.getText().toString().trim();
                     String address = webView.getUrl();
                     /*添加到本地仓库*/
-                    history.addHistory(new historyData(name,address));
+                    history.addHistory(new historyData(name, address));
                     /*添加到远程仓库*/
-                    SharedPreferences sp = mActivity.getSharedPreferences("sp_list",MODE_PRIVATE);
-                    String head = sp.getString("token","");
+                    SharedPreferences sp = mActivity.getSharedPreferences("sp_list", MODE_PRIVATE);
+                    String head = sp.getString("token", "");
 //                    if (!name.equals("百度一下")) {
 //                        httpUtils.putHistory(head, "http://139.196.180.89:8137/api/v1/histories", name, address, new Callback() {
 //                            @Override
@@ -346,29 +355,40 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
             }, 4000);
         }
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == resultCode) {
+            webView.goBack();
+        }
+    }
     private class MkWebChromeClient extends WebChromeClient {
-        private final  static int WEB_PROGRESS_MAX=100;
+        private final static int WEB_PROGRESS_MAX = 100;
+
         @Override
-        public void onProgressChanged(WebView view,int newProgress){
-            super.onProgressChanged(view,newProgress);
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
             //加载进度条，刷新进度条
             progressBar.setProgress(newProgress);
-            if(newProgress>0){
-                if(newProgress==WEB_PROGRESS_MAX){
+            if (newProgress > 0) {
+                if (newProgress == WEB_PROGRESS_MAX) {
                     progressBar.setVisibility(View.INVISIBLE);
-                }else{
+                } else {
                     progressBar.setVisibility(View.VISIBLE);
                 }
             }
         }
+
         @Override
-        public void onReceivedIcon(WebView view,Bitmap icon){
-            super.onReceivedIcon(view,icon);
+        public void onReceivedIcon(WebView view, Bitmap icon) {
+            super.onReceivedIcon(view, icon);
             webIcon.setImageBitmap(icon);
         }
+
         @Override
-        public void onReceivedTitle(WebView view,String title){
-            super.onReceivedTitle(view,title);
+        public void onReceivedTitle(WebView view, String title) {
+            super.onReceivedTitle(view, title);
             mActivity.setTitle(title);
             textUrl.setText(title);
 
@@ -378,7 +398,7 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
     }
 
 
-    public static boolean isHttpUrl(String urls){
+    public static boolean isHttpUrl(String urls) {
         boolean isUrl;
         String regex = "(((https|http)?://)?([a-z0-9]+[.])|(www.))"
                 + "\\w+[.|\\/]([a-z0-9]{0,})?[[.]([a-z0-9]{0,})]+((/[\\S&&[^,;\u4E00-\u9FA5]]+)+)?([.][a-z0-9]{0,}+|/?)";
@@ -388,6 +408,7 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
         isUrl = mat.matches();
         return isUrl;
     }
+
     private static boolean iswwwbutNOHttp(String input) {
         boolean isUrl;
         String regex = "(([a-z0-9]+[.])|(www.))"
@@ -397,10 +418,11 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
         isUrl = mat.matches();
         return isUrl;
     }
+
     /*颠倒list顺序，用户输入的信息会从上依次往下显示*/
-    private void reversedList(){
+    private void reversedList() {
         data.clear();
-        for(int i = temList.size() - 1 ; i >= 0 ; i --) {
+        for (int i = temList.size() - 1; i >= 0; i--) {
             data.add(temList.get(i));
         }
     }
@@ -409,9 +431,9 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onPause() {
         super.onPause();
-        try{
+        try {
             webView.getClass().getMethod("onPause").invoke(webView);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -421,7 +443,7 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
         super.onResume();
         try {
             webView.getClass().getMethod("onResume").invoke(webView);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -429,56 +451,56 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        if(id==R.id.goback){
-            Toast.makeText(mActivity,"3666",Toast.LENGTH_SHORT).show();
+        if (id == R.id.goback) {
+            Toast.makeText(mActivity, "3666", Toast.LENGTH_SHORT).show();
             illegWebsite.setVisibility(View.GONE);
             Liner_search.setVisibility(View.VISIBLE);
             web.setVisibility(View.VISIBLE);
         }
-        if(id==R.id.btnStart){
-            if(textUrl.hasFocus()){
+        if (id == R.id.btnStart) {
+            if (textUrl.hasFocus()) {
                 //隐藏软键盘
-                if(manager.isActive()){
+                if (manager.isActive()) {
                     manager.hideSoftInputFromWindow
-                            (textUrl.getApplicationWindowToken(),0);
+                            (textUrl.getApplicationWindowToken(), 0);
                 }
                 String input = textUrl.getText().toString();
                 sc.addRecords(input);
                 initList();
-                if(!isHttpUrl(input)){
+                if (!isHttpUrl(input)) {
                     try {
-                        input= URLEncoder.encode(input,"utf-8");
-                    }catch (UnsupportedEncodingException e){
+                        input = URLEncoder.encode(input, "utf-8");
+                    } catch (UnsupportedEncodingException e) {
                         e.printStackTrace();
                     }
-                    input="https://www.baidu.com/s?wd="+input+"&ie=UTF-8";
+                    input = "https://www.baidu.com/s?wd=" + input + "&ie=UTF-8";
                 }
-                if (iswwwbutNOHttp(input)){
+                if (iswwwbutNOHttp(input)) {
                     try {
                         // URL 编码
                         input = URLEncoder.encode(input, "utf-8");
                     } catch (UnsupportedEncodingException e) {
                         e.printStackTrace();
                     }
-                    input = "https://"+input;
+                    input = "https://" + input;
                 }
                 webView.loadUrl(input);
                 textUrl.clearFocus();
-            }else {
+            } else {
                 webView.reload();
             }
         }
-        if(id ==R.id.webIcon){
-            if (textUrl.hasFocus()){
+        if (id == R.id.webIcon) {
+            if (textUrl.hasFocus()) {
                 //隐藏软键盘
-                if(manager.isActive()){
+                if (manager.isActive()) {
                     manager.hideSoftInputFromWindow
-                            (textUrl.getApplicationWindowToken(),0);
+                            (textUrl.getApplicationWindowToken(), 0);
                 }
                 textUrl.clearFocus();
             }
         }
-        if(id == R.id.clear){
+        if (id == R.id.clear) {
             AlertDialog.Builder clearSure = new AlertDialog.Builder(mActivity);
             clearSure.setPositiveButton("确认",
                     (dialog, which) -> {
@@ -486,7 +508,7 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
                         SearchFragment.this.initList();
                     });
 
-            clearSure.setNegativeButton("取消",(dialog, which) -> dialog.dismiss());
+            clearSure.setNegativeButton("取消", (dialog, which) -> dialog.dismiss());
             clearSure.setTitle("提示");
             clearSure.setMessage("您确认清空搜索历史吗");
             clearSure.show();
--- app/src/main/java/com/webbrowser/bigwhite/activity/infoDetail.java
+++ app/src/main/java/com/webbrowser/bigwhite/activity/infoDetail.java
@@ -1,5 +1,6 @@
 package com.webbrowser.bigwhite.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,16 +12,20 @@ import com.webbrowser.bigwhite.MainActivity;
 import com.webbrowser.bigwhite.R;
 import com.webbrowser.bigwhite.View.fragment.SearchFragment;
 
-public class infoDetail extends AppCompatActivity {
+public class infoDetail extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_detail);
+        Intent intent = getIntent();
+        String s = intent.getExtras().getString("url");
+        showToast(s);
+
         Log.d("recourseUel",getClass().getSimpleName());
     }
 
     public void back(View view) {
-
+        setResult(123);
         finish();
     }
 }
